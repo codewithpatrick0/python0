89b91c7dd47ca82164c544b5abcf68d31b417a5b
Patrick <patrickcabrera2025@gmail.com>
2026-04-16 19:57:25 -0500

    add strings

---
+++ strings_and_metodos.py
@@ -0,0 +1,9 @@
+#todo Python Reaprendizaje (Strings)
+
+#¡Ejercicio 1
+#Dado un string, imprime cuántas letras tiene, en mayúsculas, en minúsculas y con la primera letra en mayúscula.
+
+string = "Mi mamá es bella"
+
+print(f"El string dado tiene :")
+print(f"{len(string.replace(" ", ""))} letras")
