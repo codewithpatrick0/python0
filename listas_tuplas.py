9e03dc38506e2a99139aac83a1e81d6d8e0496c1
Patrick <patrickcabrera2025@gmail.com>
2026-04-22 13:00:13 -0500

    listas_tuplas listo

--- listas_tuplas.py
+++ listas_tuplas.py
@@ -1,3 +1,4 @@
+
 #todo Python Reaprendizaje (Listas y Tuplas)
 
 #¡Ejercicio 1
@@ -10,6 +11,7 @@ lista_limpia = []
 for numero in lista_numeros:
     if numero not in lista_limpia:
         lista_limpia.append(numero)
+        lista_limpia.sort()
 
 print(lista_limpia)
 
@@ -30,16 +32,70 @@ print(lista_strings_largos)
 #Crea una función que reciba dos listas y retorne los elementos que tienen en común.
 
 def retornar_lista_resultante(lista1, lista2):
-    lista_resultante = []
-
-    for i in range(min(len((lista1, lista2)))):
-        if lista1[i] == lista2[i] :
-            lista_resultante.append(lista1[i])
+    
+    lista_resultante = [e for e in lista1 if e in lista2]
 
     return lista_resultante
+    
 
-lista1 = [2, 5, 10, 12, 20]
-lista2 = [10, 2, 99, 20]
+lista1_num = [2, 5, 10, 12, 20, 99]
+lista2_num = [10, 2, 99, 20,0, 0, 0, 5]
 
-resultado = retornar_lista_resultante(lista1, lista2)
+lista1_str = ["hola", "chau", "gemini"]
+lista2_str = ["gemini", "chau", "hola", "CACAS"]
+
+resultado = retornar_lista_resultante(lista1_num, lista2_num)
 print(resultado)
+
+resultado = retornar_lista_resultante(lista1_str, lista2_str)
+print(resultado)
+
+#¡Ejercicio 4 
+#Dada una lista de números, sepáralos en dos listas: pares e impares.
+
+lista_num = [2, 5, 10, 12, 7, 9]
+
+lista_pares = []
+lista_impares = []
+
+for n in lista_num :
+    if n % 2 == 0 :
+        lista_pares.append(n)
+    else :
+        lista_impares.append(n)
+        
+print(lista_impares)
+print(lista_pares)
+
+#¡Ejercicio 5
+#Crea una función que reciba una lista y retorne la lista al revés sin usar .reverse() ni slicing [::-1]
+
+def devolver_lista_al_reves(lista):
+    
+    lista_reverse = []
+    
+    i = len(lista) -                1
+    while i >= 0 :
+            lista_reverse.append(lista[i])
+            i -= 1
+            
+    return lista_reverse
+
+lista = [1, 2, "hola", 4, 5]
+
+resultado = devolver_lista_al_reves(lista)
+print(resultado)
+
+#¡Ejercicio 6
+#Dada una lista de tuplas (nombre, edad), ordénalas por edad de menor a mayor e imprímelas.
+
+lista_tuplas = [
+    ("Patrick", 19),
+    ("Luis", 52),
+    ("Jorge", 5)
+    ]
+
+lista_ordenada = sorted(lista_tuplas, key=lambda x : x[1], reverse=False)
+
+for i, elementos in enumerate(lista_ordenada):
+    print(f"{i+1}. {lista_ordenada[i]}")
