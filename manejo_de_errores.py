557c37f4a3b86aac51f8045812f83ccfd2741588
Patrick <patrickcabrera2025@gmail.com>
2026-04-25 11:50:16 -0500

    avance manejo de errores

--- manejo_de_errores.py
+++ manejo_de_errores.py
@@ -32,6 +32,82 @@ def dividir_entre_cero():
 resultado = dividir_entre_cero()
 print(resultado)
 
+#¡Ejercicio 3
+"""
+Dado un diccionario, intenta acceder a una clave que no existe 
+y maneja el error sin usar .get().
+"""
+
+libro = {
+        "nombre" : "Misery",
+        "autor" : "Stephen King",
+        "codigo" : "U24305392",
+        "precio" : 59.99,
+        "isbn" : "34987542312"
+        }
+
+try :
+    print(libro["ventas"])
+except KeyError:
+    print("No se encontró la clave")
+
+#¡Ejercicio 4
+"""
+Crea una función que reciba una lista y un índice,
+ y maneje el error si el índice no existe.
+"""
+
+def lista_indice(lista, indice):
+    try:
+        return lista[indice]
+    except IndexError:
+        return None
+    
+lista = [1, 2, 3, 4, 5, "hola", "chau"]
+
+print(lista_indice(lista, 2))
+print(lista_indice(lista, 99))
+
+#¡Ejercicio 5
+"""
+Crea una función que valide una edad — debe ser número entero y estar entre 0 y 120.
+ Lanza un ValueError con mensaje claro si no cumple.
+"""
+
+def validar_edad(edad):
+    if not 0 <= edad <= 120 :
+        raise ValueError("La edad tiene que estar en el rango entre 0 y 120 años")
+    return True
+    
+try:
+    resultado = validar_edad(122)
+    print(resultado)
+except ValueError as e:
+    print(f"Error detectado: {e}")
+
+
+#¡Ejercicio 6
+"""
+Combina todo — crea un programa que pida al usuario dos números y una operación 
+(suma, resta, multiplicación, división), maneje todos los errores posibles
+ y use finally para imprimir "Operación finalizada" siempre.
+"""
+
+def operaciones_numeros(numero1, numero2):
+    try:
+        if numero1 > 0 and numero2 > 0:
+            suma = numero1 + numero2
+            resta = numero1 - numero2
+            multiplicacion = numero1 * numero2
+            division = numero1 / numero2
+
+    except ValueError :
+        print("Los números deben ser positivos")
+
+    except ZeroDivisionError:
+        print("No se puede dividir entre 0")
+
+
 
 
 
