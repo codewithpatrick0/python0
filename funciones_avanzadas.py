b8c98c744a42a7bc19b36c27849886d0d38b1e8e
Patrick <patrickgpt23@gmail.com>
2026-05-02 18:03:36 -0500

    avance avanzadas funciones

--- funciones_avanzadas.py
+++ funciones_avanzadas.py
@@ -4,3 +4,16 @@
 #¡Ejercicio 1
 #Dada una lista de números, usa map para obtener el cuadrado de cada uno.
 
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+numeros_cuadrados = list(map(lambda x : x**2, numeros))
+print(numeros_cuadrados)
+
+#¡Ejercicio 2
+#Dada una lista de palabras, usa filter para quedarte solo con las que empiezan con vocal.
+
+palabras = ['android', 'elefante', 'moto', 'arroz', 'pato']
+
+palabras_vocal = list(filter(lambda x : x[0] in 'aeiouáéíóú', palabras))
+print(palabras_vocal)
+
